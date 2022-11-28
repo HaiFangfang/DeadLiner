5a85006db4ad8e5942d457a671ceb79f6e610311
LiangHaiyuan <2504083133@qq.com>
2022-11-28 10:59:28 +0800

    dynamic block adding

--- APP/app/src/main/java/com/example/deadliner/TaskThing.java
+++ APP/app/src/main/java/com/example/deadliner/TaskThing.java
@@ -7,52 +7,25 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link TaskThing#newInstance} factory method to
- * create an instance of this fragment.
  */
 public class TaskThing extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    private LinearLayout show;
     public TaskThing() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TaskThing.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TaskThing newInstance(String param1, String param2) {
-        TaskThing fragment = new TaskThing();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        show=(LinearLayout)getView().findViewById(R.id.showthing);
+
+
     }
 
     @Override
@@ -60,5 +33,10 @@ public class TaskThing extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_task_thing, container, false);
+    }
+    private void addView(final LinearLayout ll){
+        final TaskBlock t=TaskBlock.newInstance("1","2");
+        ll.addView(t.getView());
+
     }
 }
